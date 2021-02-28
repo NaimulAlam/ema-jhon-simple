2743cfb1ee931041e93096f57b5b9d540243867f
Naimul Alam <naimul2707@gmail.com>
2021-02-28 04:52:14 +0100

    product and shop done cart in progress

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,22 +1,33 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
-    const first10 = fakeData.slice(0, 10);
+    const first10 = fakeData.slice(0, 15);
     const [products, setProducts] = useState(first10)
+
+    const [cart, setCart] = useState([])
+
+    const handleAddProduct =(product) =>{
+        console.log('Product Added', product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
 
     return (
         <div className="shop-container">
             <div className="product-container">
-                <ul>
-                    {
-                        products.map(product => <li>{product.name}</li>)
-                    }
-                </ul>
+                {
+                    products.map(pd => <Product 
+                        handleAddProduct = {handleAddProduct}
+                        product={pd}
+                        ></Product>)
+                }
             </div>
             <div className="cart-container">
-
+                <h3>This is Cart</h3>
+                <h5>Order Summery: {cart.length}</h5>
             </div>
 
         </div>
