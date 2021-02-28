43c24433855b10b439ee41e1f98c550e7fdf522d
Naimul Alam <naimul2707@gmail.com>
2021-02-28 05:44:53 +0100

    simple try done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
@@ -26,8 +27,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h3>This is Cart</h3>
-                <h5>Order Summery: {cart.length}</h5>
+                <Cart cart={cart} ></Cart>
             </div>
 
         </div>
